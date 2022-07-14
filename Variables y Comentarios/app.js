b1d36158836d95302a0c8413af8de708f27f9ef6
Emill <lemito66@gmail.com>
2022-07-14 13:01:31 -0500

    Variables y comentarios

---
+++ Variables y Comentarios/app.js
@@ -0,0 +1,30 @@
+// Comentario una linea
+/* Comentario varias lineas */
+let a =10;
+var b =10; //
+const c =10; // este valor no va a poder cambiar
+
+//Otra forma
+let d=10,
+    e=20,
+    f=30;
+
+//  c=20 Esto no se puede hacer
+console.log('a',a)
+console.info('b',a)
+console.error('c',a)
+console.warn(a)
+//Un metodo no es más que una función que se encuentra en un objeto
+
+console.log('a',a)
+console.log('b',b)
+console.log('c',c)
+//A manera de objeto
+
+console.log({a})
+console.log({b})
+console.log({c})
+
+//Dar estilos css desde Javascript
+console.log('%c Mi texto','color:salmon; font-weight:bold')
+

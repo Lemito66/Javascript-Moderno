b3197590e3b5a2cdd6adbb815404e03b94e08884
Lemito66 <lemito66@gmail.com>
2022-07-20 14:35:04 -0500

    Operador ternario

--- Tips-Funciones-Argumentos-Desestructuracion-Objetos/assets/js/tips.js
+++ Tips-Funciones-Argumentos-Desestructuracion-Objetos/assets/js/tips.js
@@ -4,10 +4,10 @@
         apellido,
     }
 }
- */
+*/
 
 //Funcion flecha
-const crearPersona = (nombre,apellido) => ({nombre,apellido})
+const crearPersona = (nombre,apellido) => ({nombre,apellido});
 const persona=crearPersona('Emill','Logroño');
 console.log(persona);
 
@@ -24,3 +24,28 @@ const imprimeArgumentos2 = (palabra,...args) =>{ //Solo permite un argumento y l
 }
 imprimeArgumentos2('hola','pepe',25,85.5);
 //{ palabra: 'hola', args: [ 'pepe', 25, 85.5 ] }
+
+const {apellido: nuevoApellido}=crearPersona('Emill','Logroño');
+console.log({nuevoApellido});
+
+const emill = {
+    nombre: 'Emill',
+    apellido: 'Logroño',
+    vivo: false,
+    edad: 24,
+    coordenadas: {
+        lat: 34.034,
+        lng: -118.70
+    },
+    ropa: ['Hoodies','Zapatillas'],
+    'ultima-pelicula': 'Spiderman',// Si se necesita poner el signo menos se coloca en comillas
+
+};
+const imprimePropiedad = (nombrePersona)=>{
+    console.log(nombrePersona.nombre);
+    console.log(nombrePersona.apellido);
+    console.log(nombrePersona.vivo);
+    console.log(nombrePersona.edad);
+    console.log(nombrePersona.ropa);
+};
+imprimePropiedad(emill);

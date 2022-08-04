bd5c4493fef8a850779291fe86b200ba0433cc39
Emill <lemito66@gmail.com>
2022-08-04 10:00:35 -0500

    Añadiendo Todos

--- 04-todo/src/index.js
+++ 04-todo/src/index.js
@@ -1,6 +1,13 @@
-import { saludar } from './js/componentes';
 import './styles.css';
 
-const nombre = 'Fernando';
+/* import { Todo } from './classes/todo.class';
+import { TodoList } from './classes/todo-list.class'; */
 
-saludar( nombre );
+import {Todo,TodoList} from './classes';//Aqui se importa todo del archivo index.js que esta en la carpeta classes
+
+const todoList = new TodoList();
+
+const tarea = new Todo('Aprender Javascript');
+
+todoList.nuevoTodo(tarea);
+console.log(todoList);

384983122eb2b7821ea6eae5753328d31d7283a6
Lemito66 <lemito66@gmail.com>
2022-08-08 10:38:21 -0500

    async

--- 05-promesas-callbacks/src/js/promesas.js
+++ 05-promesas-callbacks/src/js/promesas.js
@@ -28,6 +28,21 @@ export const buscarHeroe = (id) => {
     });
 
 }
+//Forma Async, El async resumen todo lo que hace una promesa, por ejemplo en la función de arriba retornabamos una promesa, con async resumimos eso
+export const buscarHeroeAsync = async (id) => { // El nombre de la función no tiene nada que ver con lo que hace la función
+    const heroe = heroes[id];
+
+    //vamos a retornar la promesa
+
+    if (heroe) {
+        return heroe;//Esto envez de resolve
+    } else {
+        throw `No existe un héroe con el id ${ id }`;// Esto envez de reject
+    }
+
+
+}
+
 
 const promesaLenta = new Promise((resolve, reject) => {
     setTimeout(() => resolve('Promesa Lenta'), 2000);
@@ -36,7 +51,7 @@ const promesaMedia = new Promise((resolve, reject) => {
     setTimeout(() => resolve('Promesa Media'), 1500);
 });
 const promesaRapida = new Promise((resolve, reject) => {
-    setTimeout(() => reject('Promesa Rápida'), 1000);
+    setTimeout(() => resolve('Promesa Rápida'), 1000);
 });
 
 export {
